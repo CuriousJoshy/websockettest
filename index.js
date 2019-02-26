bbe783a53d2b8c0a6032d6e2087fc9f0d1c1fed7
CuriousJoshy <47984839+CuriousJoshy@users.noreply.github.com>
2019-02-26 08:14:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
+	console.log(__dirname,req.connection.remoteAddress);
+	
 	res.sendFile(__dirname + '/index.html');
 });
 
